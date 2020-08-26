0379f6772f6dec7928eb1a0cd2dbe18c737dfb7f
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-26 01:57:07 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -9,7 +9,10 @@ end
   # Setting these relationships up is necessary for the second step. Now that we have included relationships connecting the SightingSerializer to :bird and :location, to include attributes from those objects, the recommended method is to pass in a second options parameter to the serializer indicating that we want to include those objects:
   
   def show
-    sighting = Sighting.find(params[:id])
-    render json: SightingSerializer.new(sighting)
-  end
+  sighting = Sighting.find_by(id: params[:id])
+  options = {
+    include: [:bird, :location]
+  }
+  render json: SightingSerializer.new(sighting, options)
+end
 end
