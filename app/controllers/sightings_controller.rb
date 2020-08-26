c7794949c5247a2c9a867a7ff4c3929476ca397a
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-26 01:57:35 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -17,3 +17,46 @@ end
 end
 
 end
+
+# The result:
+
+# {
+#   "data": {
+#     "id": "2",
+#     "type": "sighting",
+#     "attributes": {
+#       "created_at": "2019-05-14T16:39:37.011Z"
+#     },
+#     "relationships": {
+#       "bird": {
+#         "data": {
+#           "id": "2",
+#           "type": "bird"
+#         }
+#       },
+#       "location": {
+#         "data": {
+#           "id": "2",
+#           "type": "location"
+#         }
+#       }
+#     }
+#   },
+#   "included": [{
+#       "id": "2",
+#       "type": "bird",
+#       "attributes": {
+#         "name": "Grackle",
+#         "species": "Quiscalus Quiscula"
+#       }
+#     },
+#     {
+#       "id": "2",
+#       "type": "location",
+#       "attributes": {
+#         "latitude": 30.26715,
+#         "longitude": -97.74306
+#       }
+#     }
+#   ]
+# }
